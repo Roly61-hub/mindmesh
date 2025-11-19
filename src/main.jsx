9acb8abb0edfb79493213b0430fe769f8ab30ff0
Roly61-hub <garyrrohloff@gmail.com>
2025-11-19 16:11:17 +1300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { TeamMode } from "./TeamMode.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TeamMode />
   </React.StrictMode>
-)
+);
