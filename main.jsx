bb5efd987ee7206689b842fe8d3e6c8df2129d4d
Roly61-hub <garyrrohloff@gmail.com>
2025-11-19 18:38:53 +1300

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,1 +1,14 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App'; ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Team from "./routes/Team";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/team" element={<Team />} />
+    </Routes>
+  </BrowserRouter>
+);
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Team from "./routes/Team";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/team" element={<Team />} />
+    </Routes>
+  </BrowserRouter>
 );
