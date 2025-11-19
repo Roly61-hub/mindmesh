3ec1f7b865eb3a424c97bb63ff7859701b664c6c
Roly61-hub <garyrrohloff@gmail.com>
2025-11-19 15:52:29 +1300

    Fix TeamMode import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TeamMode from './TeamMode.jsx'
+import { TeamMode } from "./TeamMode.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
